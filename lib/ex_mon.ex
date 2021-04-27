9f0979548ffa776f508ddb61859f8d4d8c7c9876
leonardo <leonard9@pop-os.localdomain>
2021-04-27 19:15:24 -0300

    adding new project for elixir exmon game

---
+++ lib/ex_mon.ex
@@ -0,0 +1,20 @@
+defmodule ExMon do
+
+  alias ExMon.{Game, Player}
+
+  @computer_name "Roboxilique"
+
+  def create_player(name, move_avg, move_rnd, move_heal) do
+    Player.build(name, move_rnd, move_avg, move_heal)
+  end
+
+  def start_game(player) do
+    @computer_name
+    |> create_player(:punch, :kick, :heal)
+    |> Game.start( player)
+  end
+
+  def hello do
+    :world
+  end
+end
